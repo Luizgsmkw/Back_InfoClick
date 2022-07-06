17d613fbddea231824392293ef8a3b148c38e5a2
Danrleydev <danrleysouza53@gmail.com>
2022-07-06 16:01:45 -0300

    criação do empreendedor

---
+++ src/main/java/com/api/projetoFinal/repositories/EmpreendedorRepository.java
@@ -0,0 +1,8 @@
+package com.api.projetoFinal.repositories;
+
+import com.api.projetoFinal.domain.Empreendedor;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmpreendedorRepository extends JpaRepository<Empreendedor, Integer> {
+}

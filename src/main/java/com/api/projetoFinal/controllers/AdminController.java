f4452bcdbbaa652e58ed1e1b725ee0cca1a356cf
Pâmela <pamp07@hotmail.com>
2022-07-07 10:48:27 -0300

    Criação do EndPoint findById Admin.

--- src/main/java/com/api/projetoFinal/controllers/AdminController.java
+++ src/main/java/com/api/projetoFinal/controllers/AdminController.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,4 +27,11 @@ public class AdminController {
 		List<AdminDTO> listDto = list.stream().map(adm -> new AdminDTO(adm)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
 	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<AdminDTO> findById(@PathVariable Integer id) {
+		Admin obj = service.findById(id);
+		return ResponseEntity.ok().body(new AdminDTO(obj));
+	}
+
 }

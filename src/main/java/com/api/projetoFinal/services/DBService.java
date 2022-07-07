f107174b61c088e490069daa708ae0d218140c3a
Pâmela <pamp07@hotmail.com>
2022-07-07 13:23:35 -0300

    Adição de perfil para consumidor.

--- src/main/java/com/api/projetoFinal/services/DBService.java
+++ src/main/java/com/api/projetoFinal/services/DBService.java
@@ -2,6 +2,7 @@ package com.api.projetoFinal.services;
 
 import java.util.Arrays;
 
+import com.api.projetoFinal.domain.enums.Perfil;
 import com.api.projetoFinal.domain.enums.Ramo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -26,17 +27,20 @@ public class DBService {
 	private AdminRepository adminRepository;
 
 	public void instanciaDB() {
-		Consumidor c1 = new Consumidor(null, "Gabriel", "193.019.997-06", "gabriel@gmail.com", "1234", "21992934144",
-				"2271140", "Rio de Janeiro", "Rio de Janeiro", "Curicica", "Segredo", "31");
-		Consumidor c2 = new Consumidor(null, "Mingau", "382.670.620-09", "mingau@gmail.com", "1234", "10881823033",
-				"2271140", "Rio de Janeiro", "Rio de Janeiro", "Curicica", "Segredo", "31");
+		Consumidor c1 = new Consumidor(null, "Gabriel", "193.019.997-06", "gabriel@gmail.com", "1234",
+				Perfil.CONSUMIDOR, "21992934144", "2271140", "Rio de Janeiro", "Rio de Janeiro", "Curicica", "Segredo",
+				"31");
+		Consumidor c2 = new Consumidor(null, "Mingau", "382.670.620-09", "mingau@gmail.com", "1234", Perfil.CONSUMIDOR,
+				"10881823033", "2271140", "Rio de Janeiro", "Rio de Janeiro", "Curicica", "Segredo", "31");
 
-		Empreendedor e1 = new Empreendedor(null, "Loja 1", "73.900.564/0001-54", "loja1@gmail.com","123456",
-				"21988887777",Ramo.HARDWARE ,"22720400", "Rio de Janeiro", "Rio de Janeiro", "Taquara", "maraville", "1155");
+		Empreendedor e1 = new Empreendedor(null, "Loja 1", "73.900.564/0001-54", "loja1@gmail.com", "123456",
+				"21988887777", Ramo.HARDWARE, "22720400", "Rio de Janeiro", "Rio de Janeiro", "Taquara", "maraville",
+				"1155");
 		Empreendedor e2 = new Empreendedor(null, "Loja 2", "86.005.261/0001-10", "loja2@gmail.com", "123456",
-				"21945451234",Ramo.SOFTWARE, "22720400", "Rio de Janeiro", "Rio de Janeiro", "Taquara", "Mio", "895");
-		Empreendedor e3 = new Empreendedor(null,"Loja 2", "57.050.027/0001-06", "loja3@gmail.com","123456",
-				"21992934155",Ramo.AMBOS, "22720400","Rio de Janeiro", "Rio de Janeiro", "Taquara", "maraville", "1155");
+				"21945451234", Ramo.SOFTWARE, "22720400", "Rio de Janeiro", "Rio de Janeiro", "Taquara", "Mio", "895");
+		Empreendedor e3 = new Empreendedor(null, "Loja 2", "57.050.027/0001-06", "loja3@gmail.com", "123456",
+				"21992934155", Ramo.AMBOS, "22720400", "Rio de Janeiro", "Rio de Janeiro", "Taquara", "maraville",
+				"1155");
 
 		Admin a1 = new Admin(null, "Palloma Gulliver", "gulliver.palloma@gmail.com", "p1234");
 

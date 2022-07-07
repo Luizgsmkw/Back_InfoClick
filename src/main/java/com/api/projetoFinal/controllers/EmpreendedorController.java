9f8c2508fd4909c52562dafbc5570b4e077c7cc5
fabiocortesdev <fabiocortesdev@gmail.com>
2022-07-07 08:05:32 -0300

    Crud de empreendedores

---
+++ src/main/java/com/api/projetoFinal/controllers/EmpreendedorController.java
@@ -0,0 +1,67 @@
+package com.api.projetoFinal.controllers;
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+
+import com.api.projetoFinal.domain.Empreendedor;
+
+import com.api.projetoFinal.domain.dtos.EmpreendedorDTO;
+import com.api.projetoFinal.services.EmpreendedorService;
+
+@RestController
+@RequestMapping(value = "/service/empreendedores")
+public class EmpreendedorController {
+	@Autowired
+	private EmpreendedorService service;
+	
+	@GetMapping(value = "find/{id}")
+	public ResponseEntity<Empreendedor> findById(@PathVariable Integer id) {
+		Empreendedor obj = this.service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+	 @GetMapping
+	 public ResponseEntity<List<EmpreendedorDTO>>findAll() {
+		 List<Empreendedor> list = service.findAll();
+		 List<EmpreendedorDTO> listDTO = list.stream().map(obj -> new EmpreendedorDTO(obj)).collect(Collectors.toList());
+		 return ResponseEntity.ok().body(listDTO);
+	 }
+	 
+	 @PostMapping
+	 public ResponseEntity<EmpreendedorDTO> create(@RequestBody EmpreendedorDTO objDTO) {
+		 Empreendedor newObj = service.create(objDTO);
+		 URI uri = ServletUriComponentsBuilder.fromCurrentContextPath().path("/id").buildAndExpand(newObj.getIdEmpreendedor()).toUri();
+		 return ResponseEntity.created(uri).build();
+		 
+	 }
+	 
+	  @PutMapping(value = "update/{id}")
+	    public ResponseEntity<EmpreendedorDTO> updateEmpreendedor(
+	            @PathVariable Integer id, @RequestBody EmpreendedorDTO objDto){
+	        Empreendedor obj = service.update(id, objDto);
+	        return ResponseEntity.ok().body(new EmpreendedorDTO(obj));
+	    }
+	  
+	  @DeleteMapping(value="delete/{id}")
+		public ResponseEntity<EmpreendedorDTO> delete(@PathVariable Integer id){
+			service.delete(id);
+			return ResponseEntity.noContent().build();
+		}
+
+	   
+}
--- src/main/java/com/api/projetoFinal/services/DBService.java
+++ src/main/java/com/api/projetoFinal/services/DBService.java
@@ -2,6 +2,11 @@ package com.api.projetoFinal.services;
 
 import com.api.projetoFinal.domain.Consumidor;
 import com.api.projetoFinal.repositories.ConsumidorRepository;
+
+import com.api.projetoFinal.domain.Empreendedor;
+import com.api.projetoFinal.repositories.EmpreendedorRepository;
+ 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,11 +18,19 @@ public class DBService {
 
     @Autowired
     private ConsumidorRepository consumidorRepository;
+    
+    @Autowired
+    private EmpreendedorRepository empreendedorRepository;
 
     public void instanciaDB() {
         Consumidor c1 = new Consumidor(null,"Gabriel", "193.019.997-06", "gabriel@gmail.com","1234","21992934144","2271140","Rio de Janeiro", "Rio de Janeiro", "Curicica", "Segredo", "31");
         Consumidor c2 = new Consumidor(null,"Mingau", "382.670.620-09", "mingau@gmail.com","1234","10881823033","2271140","Rio de Janeiro", "Rio de Janeiro", "Curicica", "Segredo", "31");
 
+        Empreendedor e1 = new Empreendedor(null,"Loja 1", "73.900.564/0001-54", "loja1@gmail.com", "123456", "21988887777", "22720400", "Rio de Janeiro", "Rio de Janeiro", "Taquara", "maraville", "1155");
+        Empreendedor e2 = new Empreendedor(null,"Loja 2", "86.005.261/0001-10", "loja2@gmail.com", "123456", "21945451234", "22720400", "Rio de Janeiro", "Rio de Janeiro", "Taquara", "Mio", "895");
+        
         consumidorRepository.saveAll(Arrays.asList(c1,c2));
+        empreendedorRepository.saveAll(Arrays.asList(e1,e2));
+        
     }
 }

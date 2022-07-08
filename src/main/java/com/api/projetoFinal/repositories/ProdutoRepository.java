45db6081283d1107985889bcd6eaefe3a2155b90
Danrleydev <danrleysouza53@gmail.com>
2022-07-08 09:57:42 -0300

    Criação do serviço do produto

--- src/main/java/com/api/projetoFinal/repositories/ProdutoRepository.java
+++ src/main/java/com/api/projetoFinal/repositories/ProdutoRepository.java
@@ -2,9 +2,12 @@ package com.api.projetoFinal.repositories;
 
 import com.api.projetoFinal.domain.Produto;
 
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+
+    Optional<Produto> findById(Integer id);
 
 }

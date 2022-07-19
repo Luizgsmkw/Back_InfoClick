9edb91494934ea1628ec8545ea265875188dad6b
Danrleydev <danrleysouza53@gmail.com>
2022-07-19 14:14:52 -0300

    att

--- src/main/java/com/api/projetoFinal/services/DBService.java
+++ src/main/java/com/api/projetoFinal/services/DBService.java
@@ -42,8 +42,8 @@ public class DBService {
 		Consumidor c1 = new Consumidor(null, "Gabriel", "gabriel@gmail.com", encoder.encode("123456"),"21992934144" , "2271140",
 				"Rio de Janeiro", "Rio de Janeiro", "Rio de Janeiro", "Curicica", "Segredo", "193.019.997-06");		
 		c1.addPerfil(Perfil.ADMIN);
-		Consumidor c2 = new Consumidor(null, "Mingau", "382.670.620-09", "mingau@gmail.com", encoder.encode("123456"), "10881823033",
-				"2271140", "Rio de Janeiro", "Rio de Janeiro", "Curicica", "Segredo", "382.670.620-09");
+		Consumidor c2 = new Consumidor(null, "Mingau", "mingau@gmail.com", encoder.encode("123456"), "10881823033",
+				"2271140", "Rio de Janeiro", "Rio de Janeiro", "Curicica", "Segredo","segredo", "382.670.620-09");
 
 		Empreendedor e1 = new Empreendedor(null, "Loja 1", "73.900.564/0001-54", "loja1@gmail.com", encoder.encode("123456"),
 				"21988887777", Ramo.HARDWARE, "22720400", "Rio de Janeiro", "Rio de Janeiro", "Taquara", "maraville",
